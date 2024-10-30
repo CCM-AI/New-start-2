8a76a70ba55790cfdc1de165c91d40c14bbb8049
CCM-AI <mg.almutairi@hotmail.com>
2024-10-30 04:32:07 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import openaihttps://github.com/CCM-AI/New-start-2/blob/main/app.py
 
-openai.api_key = "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"
+openai.api_key="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"
 
 try:
     response = openai.ChatCompletion.create(
